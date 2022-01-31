3f7f07179e4ffecb2aa8cf3145bfa584f676a547
HIRO15254 <hi089697@docomo.ne.jp>
2022-01-31 17:34:42 +0900

    機能を持ったUIを動的に生成
    JSXに関数などを格納した配列を渡すことによって、各モジュールを変更することなくUIを動的に生成できるように拡張

--- src/components/templates/page.tsx
+++ src/components/templates/page.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import Colors from "../../static/colors";
 import Header, { HeaderButtonData } from "../organisms/header";
-import InnerSidebar from "../organisms/inner_sidebar";
-import Sidebar from "../organisms/sidebar";
+import InnerSidebar, { InnerSidebarButtonData } from "../organisms/inner_sidebar";
+import Sidebar, { SidebarButtonData } from "../organisms/sidebar";
+import HomeIcon from "../../static/homeicon";
 
 type PageProps = {
   title: string;
   headerButtons: Array<HeaderButtonData>;
+  InnerSidebarButtons: Array<InnerSidebarButtonData>;
 };
 
 class Page extends React.Component<PageProps, Record<string, never>> {
   render() {
+    const sidebarButtons: Array<SidebarButtonData> = [
+      {
+        id: "home",
+        path: HomeIcon,
+        onClick: function () {
+          console.log("ホームボタンが押されました");
+        },
+      },
+    ];
     return (
       <div className={`flex w-screen h-screen ${Colors.THEME_1.toString("bg")} ${Colors.TEXT_2.toString("text")}`}>
-        <Sidebar />
-        <InnerSidebar />
+        <Sidebar buttons={sidebarButtons} />
+        <InnerSidebar buttons={this.props.InnerSidebarButtons} />
         <div className="flex flex-col flex-grow">
           <Header title={this.props.title} buttons={this.props.headerButtons} />
           {this.props.children}
